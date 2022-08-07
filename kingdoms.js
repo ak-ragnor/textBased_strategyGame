e57f30163b95149a207d75f3d37706e4c762a4a0
ak-ragnor <akhashsai@gmail.com>
2022-08-08 03:29:56 +0530

    updated kingdom and raid function and added new files

---
+++ kingdoms.js
@@ -0,0 +1,80 @@
+export let kingdomlist =  [
+    {
+      "no" : "1",  
+      "name" : "Nidaros",
+      "population" : "200", 
+      "soldier" : "66",
+      "gold" : "6000",
+      "popularity" : "500",
+      "posting" : "king",
+      "holdings" : ["Patricia","Sandra","Aandra"],
+      "holdedBy" : ""
+    },
+    {
+      "no" : "2",  
+      "name" : "Uppsala",
+      "population" : "150", 
+      "soldier" : "50",
+      "gold" : "5500",
+      "popularity" : "300",
+      "posting" : "duke",
+      "holdings" : ["Birka","Virka"],
+      "holdedBy" : ""
+    },
+    {
+      "no" : "3",  
+      "name" : "Patricia",
+      "population" : "50", 
+      "soldier" : "15",
+      "gold" : "550",
+      "popularity" : "15",
+      "posting" : "commander",
+      "holdings" : [],
+      "holdedBy" : "Nidaros"
+    },
+    {
+      "no" : "4",
+      "name" : "Sandra",
+      "population" : "40", 
+      "soldier" : "13",
+      "gold" : "500",
+      "popularity" : "12",
+      "posting" : "commander",
+      "holdings" : [],
+      "holdedBy" : "Nidaros"
+    },
+    {
+      "no" : "5",
+      "name" : "Birka",
+      "population" : "30", 
+      "soldier" : "10",
+      "gold" : "400",
+      "popularity" : "10",
+      "posting" : "commander",
+      "holdings" : [],
+      "holdedBy" : "Uppsala"
+    },
+    {
+      "no" : "6",
+      "name" : "Virka",
+      "population" : "30", 
+      "soldier" : "10",
+      "gold" : "400",
+      "popularity" : "10",
+      "posting" : "commander",
+      "holdings" : [],
+      "holdedBy" : "Uppsala"
+    },
+    {
+      "no" : "7",
+      "name" : "Aandra",
+      "population" : "35", 
+      "soldier" : "9",
+      "gold" : "400",
+      "popularity" : "10",
+      "posting" : "commander",
+      "holdings" : [],
+      "holdedBy" : "Nidaros"
+    },
+  
+  ]
---
+++ village.js
@@ -0,0 +1,115 @@
+export let villageList =[
+    {
+        "id" : 1,
+        "name" : "Abdera",
+        "gold" : 100,
+        "battle" : 5,
+        "attacked" : 0,
+        "og": 100,
+        "popularity" : 5,
+        "day" : 1,
+        "attackDay" : 0   
+    },
+    {
+        "id" : 2,
+        "name" : "Acharnae",
+        "gold" : 200,
+        "battle" : 6,
+        "attacked" : 0,
+        "og": 200,
+        "popularity" : 7,
+        "day" : 1,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 3,
+        "name" : "Asine",
+        "gold" : 100,
+        "battle" : 5,
+        "attacked" : 0,
+        "og": 100,
+        "popularity" : 10,
+        "day" : 2,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 4,
+        "name" : "Halieis",
+        "gold" : 200,
+        "battle" : 12,
+        "attacked" : 0,
+        "og": 200,
+        "popularity" : 20,
+        "day" : 2,
+        "attackDay" : 0    
+    }, 
+    {
+        "id" : 5,
+        "name" : "Ialysos",
+        "gold" : 300,
+        "battle" : 15,
+        "attacked" : 0,
+        "og": 300,
+        "popularity" : 30,
+        "day" : 3,
+        "attackDay" : 0    
+    }, 
+    {
+        "id" : 6,
+        "name" : "Juktas",
+        "gold" : 150,
+        "battle" : 8,
+        "attacked" : 0,
+        "og": 150,
+        "popularity" : 10,
+        "day" : 2,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 7,
+        "name" : "Issa",
+        "gold" : 200,
+        "battle" : 10,
+        "attacked" : 0,
+        "og": 200,
+        "popularity" : 15,
+        "day" : 3,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 8,
+        "name" : "Kaiete",
+        "gold" : 200,
+        "battle" : 8,
+        "attacked" : 0,
+        "og": 200,
+        "popularity" : 10,
+        "day" : 3,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 9,
+        "name" : "Katane",
+        "gold" : 500,
+        "battle" : 25,
+        "attacked" : 0,
+        "og": 500,
+        "popularity" : 50,
+        "day" : 5,
+        "attackDay" : 0    
+    },
+    {
+        "id" : 10,
+        "name" : "Lampsacus",
+        "gold" : 700,
+        "battle" : 35,
+        "attacked" : 0,
+        "og": 700,
+        "popularity" : 80,
+        "day" : 5,
+        "attackDay" : 0    
+    },
+
+    
+    
+]
